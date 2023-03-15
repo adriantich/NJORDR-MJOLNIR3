194b6525adb35b6ce0d66d9b8b0e950fff289ffe
adriantich <adriantich@gmail.com>
2023-03-15 16:28:55 +0100

    no zero taxid

--- COInr_to_taxdump_complete.R
+++ COInr_to_taxdump_complete.R
@@ -45,8 +45,8 @@ merged_dmp <- read.table(opt$merged,
 print(paste("dimensions :",dim(coinr_data)))
 
 # take only the negative Taxids
-coinr_data_neg <- coinr_data[coinr_data$tax_id < 0,]
-coinr_data <- coinr_data[!coinr_data$tax_id < 0,]
+coinr_data_neg <- coinr_data[coinr_data$tax_id <= 0,]
+coinr_data <- coinr_data[coinr_data$tax_id > 0,]
 print(paste(dim(coinr_data_neg)[1],"negative taxids"))
 coinr_data_neg$tax_id <- abs(coinr_data_neg$tax_id) + opt$new_taxids
 coinr_data_neg$parent_tax_id[coinr_data_neg$parent_tax_id<0] <- abs(coinr_data_neg$parent_tax_id[coinr_data_neg$parent_tax_id<0]) + opt$new_taxids
