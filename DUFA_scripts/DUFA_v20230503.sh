d498c6fe19281cafeda01ed57a3de1897693503a
AdriAntich <adriantich@gmail.com>
2023-05-12 13:13:17 +0200

    duplicates removed

--- DUFA_scripts/DUFA_v20230503.sh
+++ DUFA_scripts/DUFA_v20230503.sh
@@ -8,18 +8,18 @@
 
 conda activate mkcoinr
 
-# scripts_COInr=~/SOFT/mkCOInr/scripts/
-scripts_COInr=~/mkCOInr/scripts/
-# scripts_DUFA=~/NJORDR-MJOLNIR3/DUFA_scripts/
-scripts_DUFA=~/Nextcloud/2_PROJECTES/NJORDR-MJOLNIR3/DUFA_scripts/
+scripts_COInr=~/SOFT/mkCOInr/scripts/
+# scripts_COInr=~/mkCOInr/scripts/
+scripts_DUFA=~/NJORDR-MJOLNIR3/DUFA_scripts/
+# scripts_DUFA=~/Nextcloud/2_PROJECTES/NJORDR-MJOLNIR3/DUFA_scripts/
 forward=GGWACWRGWTGRACWNTNTAYCCYCC
 forward="-fw \"${forward}\""
 # reverse=TANACYTCNGGRTGNCCRAARAAYCA
 reverse=''
 Date=2022_05_06
 # add_seqs=DUFA_scripts/Additional_seqs.tsv
-add_seqs=Additional_seqs.tsv
-out_dir=~/TAXO/
+# add_seqs=Additional_seqs.tsv
+out_dir=~/TAXO_DUFA/
 
 # first download the COInr database. In this case only the taxonomy file will be used.
 
@@ -32,8 +32,9 @@ rm COInr_${Date}.tar.gz
 mv COInr_${Date} COInr
 
 # create new files in the correct format
-Rscript ${scripts_DUFA}NJORDR_0.1_format_additional_seqs.R -i ${scripts_DUFA} -o ~/TAXO/
-cat COInr/taxonomy.tsv ${add_seqs_dir}taxo_2join.tsv >taxonomy.tsv
+Rscript ${scripts_DUFA}NJORDR_0.1_format_additional_seqs.R -i ${scripts_DUFA} -o ${out_dir}
+# cat COInr/taxonomy.tsv ${add_seqs_dir}taxo_2join.tsv >taxonomy.tsv
+cat COInr/taxonomy.tsv ${out_dir}taxo_2join.tsv >taxonomy.tsv
 
 # select region
 perl ${scripts_COInr}select_region.pl -tsv DUFA.tsv -outdir . -e_pcr 1 ${forward} ${reverse} -identity 0 -tcov 0 -min_amplicon_length 299 -max_amplicon_length 320
